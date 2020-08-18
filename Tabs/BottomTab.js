aec6653a077eeb1dc26ef236392d755be38b026e
vimal Raveendran <vimalraveendra@gmail.com>
2020-08-18 13:35:50 +0200

    Added menu Icon to the Stack HomeScreen component for UI/UX, also added icons to the BottomTabs

--- Tabs/BottomTab.js
+++ Tabs/BottomTab.js
@@ -5,19 +5,35 @@ import Tab2 from './Tab2';
 import Tab3 from './Tab3';
 
 const MaterialBottomTab = createMaterialBottomTabNavigator();
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const BottomTab = () => {
   return (
     <MaterialBottomTab.Navigator>
       <MaterialBottomTab.Screen
         name="Tab1"
-        children={Tab1}
+        component={Tab1}
         options={{
-          title: 'Home',
+          tabBarLabel: 'Home',
+          tabBarIcon: () => <Icon name="ios-home" size={25} />,
         }}
       />
-      <MaterialBottomTab.Screen name="Tab2" component={Tab2} />
-      <MaterialBottomTab.Screen name="Tab3" component={Tab3} />
+      <MaterialBottomTab.Screen
+        name="Tab2"
+        component={Tab2}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: () => <Icon name="ios-profile" size={25} />,
+        }}
+      />
+      <MaterialBottomTab.Screen
+        name="Tab3"
+        component={Tab3}
+        options={{
+          tabBarLabel: 'Notifications',
+          tabBarIcon: () => <Icon name="ios-notifications" size={25} />,
+        }}
+      />
     </MaterialBottomTab.Navigator>
   );
 };
