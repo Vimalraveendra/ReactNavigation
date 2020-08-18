6af1e9601fbcc048524a30673354297a33d2a183
vimal Raveendran <vimalraveendra@gmail.com>
2020-08-18 17:21:44 +0200

    added assests folder and Updated Drawer content with Image,View & styles

--- Drawer/DrawerContent.js
+++ Drawer/DrawerContent.js
@@ -13,16 +13,51 @@ import {
 } from 'react-native-paper';
 
 import {
-  createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
 } from '@react-navigation/drawer';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 const DrawerContent = (props) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Drawer content</Text>
+      <DrawerContentScrollView {...props}>
+        <View style={styles.drawerContent}>
+          <View style={styles.userInfoContent}>
+            <View style={styles.headContent}>
+              <Avatar.Image
+                source={require('../assests/myimage.png')}
+                size={50}
+              />
+              <View style={styles.titleContent}>
+                <Title style={styles.title}>Vimal Kumar</Title>
+                <Caption sytle={styles.caption}>@it's Me</Caption>
+              </View>
+            </View>
+            <View style={styles.followingContent}>
+              <View>
+                <Paragraph>80</Paragraph>
+                <Caption>Following</Caption>
+              </View>
+              <View>
+                <Paragraph>100</Paragraph>
+                <Caption>Followers</Caption>
+              </View>
+            </View>
+          </View>
+        </View>
+      </DrawerContentScrollView>
+      <Drawer.Section style={styles.bottomDrawerSection}>
+        <DrawerItem
+          icon={({color, size}) => {
+            <Icon name="exit-to-app" color={color} size={size} />;
+          }}
+          label="sign-out"
+          onPress={() => {}}
+        />
+      </Drawer.Section>
     </View>
   );
 };
@@ -33,8 +68,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 25,
+  headContent: {
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  titleContent: {
+    marginLeft: 15,
+  },
+
+  followingContent: {
+    flexDirection: 'row',
   },
 });
 
